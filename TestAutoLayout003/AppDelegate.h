9bdef2ea9c0e43794b802e0b9d38c802294cc21a
dev <SQW@shenqinwei.local>
2015-05-22 09:58:00 +0800

    Initial Commit

---
+++ TestAutoLayout003/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestAutoLayout003
+//
+//  Created by dev on 15/5/22.
+//  Copyright (c) 2015年 dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestAutoLayout003/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestAutoLayout003
+//
+//  Created by dev on 15/5/22.
+//  Copyright (c) 2015年 dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
